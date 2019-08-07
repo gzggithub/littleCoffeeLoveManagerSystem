f323c8682c4d0e819f7ecacbfa2491874fffbdc9
zhiguo <zhiguo.guo@joyreach.com>
2019-08-07 16:25:15 +0800

    日期国际化中文全局配置

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
     Route
 } from 'react-router-dom';
 import {LocaleProvider} from 'antd';
+import {ConfigProvider} from 'antd';
 import zh_CN from 'antd/lib/locale-provider/zh_CN';
 import 'moment/locale/zh-cn';
 import './App.css';
@@ -13,14 +14,14 @@ import Login from './components/Login';
 class App extends Component {
     render() {
         return (
-            <LocaleProvider locale={zh_CN}>
+            <ConfigProvider locale={zh_CN}>
                 <Router>
                     <div className="App">
                         <Route exact path="/" component={Login}/>
                         <Route path="/index" component={Home}/>
                     </div>
                 </Router>
-            </LocaleProvider>
+            </ConfigProvider>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -4,15 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// import { ConfigProvider } from 'antd';
-import { LocaleProvider } from 'antd';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
-// import zhCN from 'antd/es/locale/zh_CN';
-import moment from 'moment';
-import 'moment/locale/zh-cn';
-
-moment.locale('zh-cn');
-
-// ReactDOM.render(<ConfigProvider locale={zhCN}><App /></ConfigProvider>, document.getElementById('root'));
-ReactDOM.render(<LocaleProvider locale={zhCN}><App /></LocaleProvider>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
